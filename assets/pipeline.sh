a9ebfee829dff14dadba7aa69eba2d93cf804c9d
wv9 <wv9@lab-rg07-12.cms.waikato.ac.nz>
2022-05-21 12:37:30 +1200

    COMPX341-22A3 Fixing compile-time error

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,3 +1,34 @@
+if [[ $# -eq 0 ]] ; then
+    echo "No commit message given"
+    exit 1
+fi
+
+echo "(0) Preparing the Pipeline"
+cd /home/wv9/Year3/341/A3/COMPX341-A3/assets
+
+echo "(1) Install"
 npm install 
-npm run build 
-npm run start 
+
+echo "(2) Build (Compile the application)"
+if npm run build; then
+    echo "Build succeded"
+    
+else
+    echo "Build failed, exiting pipeline"
+    exit
+fi
+
+echo "(3) Commit to git"
+cd /home/wv9/Year3/341/A3/COMPX341-A3
+git add --all
+git commit -m "$1"
+git push origin
+cd /home/wv9/Year3/341/A3/COMPX341-A3/assets
+
+echo "(4) Deploy the application"
+if npm run start; then
+    echo "Deployed successfully"
+else
+    echo "Deploy failed, exiting pipeline"
+    exit
+fi
